ae8a63a5932b72bb4ca0131b00c053434301f8cd
8bit/8bai <91180750+8bai@users.noreply.github.com>
2022-04-25 23:23:47 +0900

    ハンバーガー付けました

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 jQuery(function($){
     console.log('Hello!');
-    $('.myface img').mouseover(function() {
-        $(this).css('border', '2px solid black');
+    /*
+    $('.myface img').click(function() {
+        $('.profile').animate({fontSize: 80}, 2000, 'swing', function(){alert("きも");});
     });
-    $('.myface img').mouseout(function() {
-        $(this).css('border', '2px solid white');
+    $('.myface img').click(function() {
+        $(this).fadeOut('slow');
+    });
+    */
+
+    $('.humberger').click(function() {
+        $('#ketsu').toggleClass('open');
+    });
+    $('.batsu').click(function() {
+        $('#ketsu').toggleClass('open');
+        
+    });
+    
+    $(window).scroll(function() {
+        
+        var scroll = $(this).scrollTop();
+        
+        if(scroll > 300){
+            $('#gototop').fadeIn('slow');
+        }else{
+            $('#gototop').fadeOut('slow');
+        }
     });
 });
